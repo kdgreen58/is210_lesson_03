fad0b6cbc2e3dee7679583447ea92dd16827b4bc
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-14 23:44:15 -0400

    My task 04 submission.

---
+++ task_04.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Task 04. Alarm Clock'''
+
+WEEKDAY = (raw_input('What day of the week is it? ').lower())[:3]
+
+CURTIME = int(raw_input('What time is it? '))
+
+if WEEKDAY == 'sat' or WEEKDAY == 'sun' or CURTIME < 600:
+    SNOOZE = True
+else:
+    SNOOZE = False
+if SNOOZE == False:
+    print ('BEEP! ' * 5)
