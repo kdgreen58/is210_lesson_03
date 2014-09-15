7d1ee1bcd25d1f34c4babfd522904b7b2a31afaf
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-14 22:11:41 -0400

    My task 01 submission.

--- task_01.py
+++ task_01.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-''' My task 01 docstring. Asks two questions.
-One of which stores as an integer. ''''
-
+""" My task 01 docstring. Asks two questions. """
 
 QUESTION_01 = raw_input('Can I ask you a question? ')
 
