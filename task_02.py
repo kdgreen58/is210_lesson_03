48e417d6b98333f03ec4b8c7b754c522fb9f3018
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-14 22:42:23 -0400

    My task 02 submission.

--- task_02.py
+++ task_02.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 """Code for Task 02. Blood Pressure input."""
 
-USERINPUT = int(raw_input('What is your blood pressure? ')
+USERINPUT = int(raw_input('What is your blood pressure? '))
 
 if USERINPUT >= 160:
-                BP_STATUS = 'Emergency'
+    BP_STATUS = 'Emergency'
 elif USERINPUT >= 140 and USERINPUT <= 159:
-                BP_STATUS = 'High'
+    BP_STATUS = 'High'
 elif USERINPUT >= 120 and USERINPUT <= 139:
-                BP_STATUS = 'Warning'
+    BP_STATUS = 'Warning'
 elif USERINPUT >= 90 and USERINPUT <= 119:
-                BP_STATUS = 'Ideal'
+    BP_STATUS = 'Ideal'
 else:
-                BP_STATUS = 'Low'
+    BP_STATUS = 'Low'
 
-print 'Your status is currently: {0}!'.format(BP_STATUS)           
+print 'Your status is currently: {0}!'.format(BP_STATUS)          
