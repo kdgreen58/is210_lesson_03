96cdd82bd14ffd8a41bb396483d8b173295dd0f5
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-14 22:09:30 -0400

    My task 01 submission.

--- task_01.py
+++ task_01.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+''' My task 01 docstring. Asks two questions.
+One of which stores as an integer. ''''
+
+
 QUESTION_01 = raw_input('Can I ask you a question? ')
 
-QUESTION_02 = int (input('What is your age? '))
+QUESTION_02 = int(raw_input('What is your age? '))
