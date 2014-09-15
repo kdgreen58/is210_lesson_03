2f041d1751523a4a6e8150dece1b04b141093dfd
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-15 01:59:10 -0400

    My task 05 submission.

--- task_05.py
+++ task_05.py
@@ -12,21 +12,21 @@ QUALIFIED = raw_input('Are you pre-qualified for this loan? ')
 if AMOUNT >= 0 and AMOUNT <= 199999:
     if YEARS >= 1 and YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0363') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0363') / 12)) **
                        (12 * YEARS))
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
             TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0465') / 12)) **
                         (12 * YEARS))
     elif YEARS >= 16 and YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0404') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0404') / 12)) **
                        (12 * YEARS))
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
             TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0498') / 12)) **
                         (12 * YEARS))
     elif YEARS >= 21 and YEARS <= 30:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0577') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0577') / 12)) **
                        (12 * YEARS))
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
             TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0639') / 12)) **
@@ -36,21 +36,21 @@ if AMOUNT >= 0 and AMOUNT <= 199999:
 elif AMOUNT >= 200000 and AMOUNT <= 999999:
     if YEARS >= 1 and YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0302') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0302') / 12)) **
                        (12 * YEARS))
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
             TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0398') / 12)) **
                         (12 * YEARS))
     elif YEARS >= 16 and YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0327') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0327') / 12)) **
                        (12 * YEARS))
         elif QUALIFIED == 'No' or QUALIFIED == 'n':
             TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0408') / 12)) **
                         (12 * YEARS))
     elif YEARS >= 21 and YEARS <= 30:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0466') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0466') / 12)) **
                        (12 * YEARS))
         else:
             TOTAL = None
@@ -59,13 +59,13 @@ elif AMOUNT >= 200000 and AMOUNT <= 999999:
 elif AMOUNT >= 1000000:
     if YEARS >= 1 and YEARS <= 15:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0205') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0205') / 12)) **
                        (12 * YEARS))
         else:
             TOTAL = None
     elif YEARS >= 16 and YEARS <= 20:
         if QUALIFIED == 'Yes' or QUALIFIED == 'y':
-           TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0262') / 12)) **
+            TOTAL = int(AMOUNT * (1 + (decimal.Decimal('.0262') / 12)) **
                        (12 * YEARS))
         else:
             TOTAL = None
