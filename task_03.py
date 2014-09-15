77de1772c659a7939775cdd5b9ee1df7f9cc69e8
Kolton Green <kolton.green@spsmail.cuny.edu>
2014-09-14 23:06:30 -0400

    My task 03 submission.

---
+++ task_03.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Task 03. Paint choices.'''
+
+COLOR = raw_input('Which base color, Seattle Gray or Manatee? ')
+
+if COLOR == 'Seattle Gray':
+    ACCENT = raw_input('''Which accent color, Ceramic Glaze or
+    Cumulus Nimbus? ''')
+    
+    if ACCENT == 'Ceramic Glaze':
+        HIGHLIGHT = raw_input('''Which highlight color, Basically White
+        or White? ''')
+        
+    elif ACCENT == 'Cumulus Nimbus':
+        HIGHLIGHT = raw_input('''Which highlight color, Off-White or
+        Paper White''')
+        
+elif COLOR == 'Manatee':
+    ACCENT = raw_input('''Which accent color, Platinum Mist or
+    Spartan Sage? ''')
+    
+    if ACCENT == 'Platinum Mist':
+        HIGHLIGHT = raw_input('''Which highlight color, Bone White
+        or Just White? ''')
+        
+    elif ACCENT == 'Spartan Sage':
+        HIGHLIGHT = raw_input('''Which highlight color, Fractal White or
+        Not White''')
+
+print 'Your selected colors are, {0}, {1} and {2}'.format(COLOR,ACCENT,HIGHLIGHT)
